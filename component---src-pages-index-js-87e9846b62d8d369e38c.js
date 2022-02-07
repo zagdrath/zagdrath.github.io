130a4072e855524aef14f058449641b2eebcb438
Cody Wellman <zagdrath@member.fsf.org>
2022-02-07 17:01:58 -0500

    Clean out old files

--- component---src-pages-index-js-87e9846b62d8d369e38c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkzagdrath_old=self.webpackChunkzagdrath_old||[]).push([[678],{6558:function(e,t,l){l.r(t);var n=l(7294),a=l(5414),r=l(7306),c=l(1619);t.default=function(){return n.createElement("body",null,n.createElement(a.q,null,n.createElement("title",null,"Home - Zagdrath"),n.createElement("meta",{charset:"UTF-8"})),n.createElement(r.Z,null),n.createElement("article",null,n.createElement("p",null,"Welcome to my 90s styled personal site!")),n.createElement(c.Z,null))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-87e9846b62d8d369e38c.js.map
